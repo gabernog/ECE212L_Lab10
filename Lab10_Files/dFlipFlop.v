68e048c464eae51095a11071b5a72a254c0e54aa
gabernog <90170099+gabernog@users.noreply.github.com>
2026-04-02 15:38:15 -0600

    Update dFlipFlop.v

--- Lab10_Files/dFlipFlop.v
+++ Lab10_Files/dFlipFlop.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/02/2026 12:47:29 PM
-// Design Name: 
-// Module Name: dFlipFlop
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module dFlipFlop(
   input clk,
