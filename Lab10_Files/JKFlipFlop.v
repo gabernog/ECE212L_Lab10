729b44a1c859f00f4a69d373cb1a04d6bffe7db8
gabernog <90170099+gabernog@users.noreply.github.com>
2026-04-02 15:37:55 -0600

    Update JKFlipFlop.v

--- Lab10_Files/JKFlipFlop.v
+++ Lab10_Files/JKFlipFlop.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/02/2026 12:47:52 PM
-// Design Name: 
-// Module Name: JKFlipFlop
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module JKFlipFlop(
     input clk,
